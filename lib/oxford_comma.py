b49ed92e4f072f7f35237f87d3bfa4e7e4dfa888
RegisLewis <regisaslewis@gmail.com>
2023-06-03 23:30:52 -0700

    oxford comma lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,11 @@
 def oxford_comma(items):
-    return None
+    if len(items) > 2:
+        items.insert(-1, "and")
+        string = ", ".join(items)
+        return string.replace("and,", "and")
+    elif len(items) == 2:
+        items.insert(-1, "and")
+        string = " ".join(items)
+        return string
+    else:
+        return "".join(items)
